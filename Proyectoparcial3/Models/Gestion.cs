5c336bc21eadf60fcb1af8f89a90075f93a6f898
jgalvissaavedra <jgalvissaavedra@gmail.com>
2021-11-11 11:55:17 -0400

    Agregar archivos de proyecto.

---
+++ Proyectoparcial3/Models/Gestion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Proyectoparcial3.Models
+{
+    public class Gestion
+    {
+        [Key]
+        public string Almacen { get; set; }
+        [Display(Name = "Nombre del almacen")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "El nombre debe contener entre 3 a 60 caracteres")]
+        [Required(ErrorMessage = "yes")]
+        public string Producto { get; set; }
+        [Display(Name = "nombre del producto")]
+        public string Precio { get; set; }
+        [Url]
+        [Display(Name = "link")]
+        public string link { get; set; }
+
+    }
+}
