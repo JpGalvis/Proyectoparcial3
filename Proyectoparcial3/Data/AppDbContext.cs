04eef3ca78eaef4244c5a4a32607817e817cd3e2
jgalvissaavedra <jgalvissaavedra@gmail.com>
2021-11-11 12:04:06 -0400

    data

---
+++ Proyectoparcial3/Data/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Proyectoparcial3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Proyectoparcial3.Data
+{
+    public class AppDbContext : DbContext
+
+    {
+
+
+        public DbSet<Gestion> Gestiones { get; set; } //Llama al modelo (Person) 
+
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+
+        {
+
+
+
+        }
+
+    }
+}
+
